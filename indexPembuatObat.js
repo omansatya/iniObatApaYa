40f6ba78488732022ce77f97c70b35fd330f2615
rahmatziadiugi <rahmatziadiugi@gmail.com>
2022-04-21 20:33:18 +0700

    add id input, add dom for admin

---
+++ indexPembuatObat.js
@@ -0,0 +1,12 @@
+document.getElementById("tambahObatButton").addEventListener("click", tambahObat);   
+function tambahObat() {    
+    // addObat() ??
+    // namaObat = document.getElementById('addNamaObat').value
+    // jenisObat = document.getElementById('addJenisObat').value
+    // indexObat = document.getElementById('addIndexObat').value 
+    // bentukObat = document.getElementById('addBentukObat').value
+    // warnaObat = document.getElementById('addWarnaObat').value 
+    // manfaatObat = document.getElementById('addManfaatObat').value
+    // gambar = document.getElementById('addGambar').value
+    alert('tambah')
+}
