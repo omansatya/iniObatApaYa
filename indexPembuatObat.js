403dcb2cf47bcc815d04863c06bceadc4de00b79
rahmatziadiugi <rahmatziadiugi@gmail.com>
2022-04-22 04:56:14 +0700

    add reset field

--- indexPembuatObat.js
+++ indexPembuatObat.js
@@ -1,116 +1,38 @@
 document.getElementById("tambahObatButton").addEventListener("click", tambahObat);   
-function tambahObat() {    
-    // addObat() ??
-    // namaObat = document.getElementById('addNamaObat').value
-    // jenisObat = document.getElementById('addJenisObat').value
-    // indexObat = document.getElementById('addIndexObat').value 
-    // bentukObat = document.getElementById('addBentukObat').value
-    // warnaObat = document.getElementById('addWarnaObat').value 
-    // manfaatObat = document.getElementById('addManfaatObat').value
-    // gambar = document.getElementById('addGambar').value
-    alert('tambah')
+function tambahObat() {        
+    namaObat = document.getElementById('addNamaObat').value
+    jenisObat = document.getElementById('addJenisObat').value
+    indexObat = document.getElementById('addIndexObat').value 
+    bentukObat = document.getElementById('addBentukObat').value
+    warnaObat = document.getElementById('addWarnaObat').value 
+    manfaatObat = document.getElementById('addManfaatObat').value
+    gambar = document.getElementById('addGambar').value
+
+    // check if req is empty
+    if(!namaObat || !jenisObat || !indexObat || !bentukObat || !warnaObat || !manfaatObat){
+        alert('input kurang')
+    }
+    else{
+        // check if dupe or fail
+        if(false){
+            alert('data sudah ada')
+        }
+        else{
+            alert('berhasil ditambahkan')
+            // call func add to db
+            
+            resetField()            
+        }
+    }
 }
 
-
-// DATABASE OBAT
-
-let listObat = [{
-    nama: 'Captopril',
-    bentuk: 'round',
-    warna: 'white',
-    manfaat: ['Mengatasi hipertensi dan gagal jantung', 'Mencegah komplikasi pascaserangan jantung', 'Mengobati nefropati diabetik'],
-    usia: ['anak', 'dewasa', 'lansia'],
-    gambar: 'src/captopril.jpg',
-    penyakit: ['Hipertensi', 'Gagal jantung', 'Nefropati diabetik'],
-    inskripsi: ['N', '133']
-}, 
-{
-    nama: 'Amoxicillin',
-    bentuk: 'egg-shape',
-    warna: 'white',
-    manfaat: ['Mengatasi infeksi bakteri', 'Mengatasi gonore', 'Mengobati pielonefritis'],
-    usia: ['anak', 'dewasa'],
-    gambar: 'src/amoxicillin.jpg',
-    penyakit: ['Infeksi bakteri', 'Gonore', 'Pielonefritis'],
-    inskripsi: ['221']
-}, 
-{
-    nama: 'Simvastanin',
-    bentuk: 'round',
-    warna: 'yellow',
-    manfaat: ['Menurunkan kadar kolesterol dalam darah'],
-    usia: ['anak', 'dewasa', ],
-    gambar: 'src/simvastanin.jpg',
-    penyakit: ['Kolesterol'],
-    inskripsi: ['93']
-}, 
-{
-    nama: 'Tramadol Hydrocloride',
-    bentuk: 'egg-shape',
-    warna: 'white',
-    manfaat: ['Meredakan nyeri'],
-    usia: ['dewasa'],
-    gambar: 'src/tramadol-hydrocloride.jpg',
-    penyakit: ['Nyeri'],
-    inskripsi: ['377']
-}, 
-{
-    nama: 'Aspirin',
-    bentuk: 'round',
-    warna: 'yellow',
-    manfaat: ['Meredakan nyeri,Meredakan demam,Meredakan peradangan,Mencegah terbentuknya gumpalan darah'],
-    usia: ['dewasa'],
-    gambar: 'src/aspirin.jpg',
-    penyakit: ['Nyeri', 'Demam', 'Peradangan', 'Penggumpalan darah'],
-    inskripsi: ['03', 'SP']
-}, 
-{
-    nama: 'Levothyroxine',
-    bentuk: 'capsule',
-    warna: 'blue',
-    manfaat: ['Mengobati hipotiroidisme'],
-    usia: ['dewasa'],
-    gambar: 'src/levothyroxine.jpg',
-    penyakit: ['Hipotiroidisme'],
-    inskripsi: ['137', 'GG', '330']
-}, 
-{
-    nama: 'Metformin',
-    bentuk: 'egg-shape',
-    warna: 'white',
-    manfaat: ['Menurunan kadar gula darah pada penderita diabetes tipe 2'],
-    usia: ['anak', 'dewasa'],
-    gambar: 'src/metformin.jpg',
-    penyakit: ['Diabetes'],
-    inskripsi: ['Andrx', '676']
-}, 
-{
-    nama: 'Olanzapine',
-    bentuk: 'eliptical',
-    warna: 'yellow',
-    manfaat: ['	Mengobati skizofrenia dan gangguan bipolar'],
-    usia: ['dewasa'],
-    gambar: 'src/olanzapine.jpg',
-    penyakit: ['Skizofernia', 'Bipolar'],
-    inskripsi: ['15']
-}, 
-{
-    nama: 'Ranitidine Hydrochloride',
-    bentuk: 'capsule',
-    warna: 'white',
-    manfaat: ['	Menurunkan produksi asam lambung berlebih'],
-    usia: ['anak', 'dewasa'],
-    gambar: 'src/ranitidine-hydrochloride.jpg',
-    penyakit: ['Asam lambung'],
-    inskripsi: ['0031']
-}, 
-{
-    nama: 'Clobazam',
-    bentuk: 'capsule',
-    warna: 'white',
-    manfaat: ['Mengatasi kejang pada epilepsi'],
-    usia: ['anak', 'dewasa'],
-    gambar: 'src/clobazam.jpg',
-    penyakit: ['Epilepsi'],
-    inskripsi: ['13', '25']
-}]
+function resetField() {
+    // clear field
+    document.getElementById('addNamaObat').value = ''
+    document.getElementById('addJenisObat').value = ''
+    document.getElementById('addIndexObat').value = ''
+    document.getElementById('addBentukObat').value = ''
+    document.getElementById('addWarnaObat').value = ''
+    document.getElementById('addManfaatObat').value = ''
+    document.getElementById('addGambar').value = ''
+}
