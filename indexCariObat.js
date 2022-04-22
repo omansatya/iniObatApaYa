6321cf5269d3db0dfd56a26221270d3ad26083ec
rahmatziadiugi <rahmatziadiugi@gmail.com>
2022-04-22 11:10:12 +0700

    dom cari obat

--- indexCariObat.js
+++ indexCariObat.js
@@ -1,10 +1,11 @@
 document.getElementById("cariObatButton").addEventListener("click", cariObat);   
-function carihObat() {        
-    indexObat = document.getElementById('indexObat').value 
-    bentukObat = document.getElementById('bentukObat').value
-    warnaObat = document.getElementById('warnaObat').value 
+function cariObat() {        
+    indexObat = document.getElementById('obatInskripsi').value 
+    bentukObat = document.getElementById('obatBentuk').value
+    warnaObat = document.getElementById('obatWarna').value 
     penyakit = document.getElementById('penyakitObat').value
 
+    // creat obj obat to search
     let obat = {}
     if(indexObat!==''){
         obat['inskripsi'] = indexObat
@@ -20,20 +21,74 @@ function carihObat() {
     }    
     
     // call filter obat
-    const obatDitemukan = filterObat(obat, '')
+    const obatDitemukan = filterObat(obat)
 
-    if (obatDitemukan.length === 1) {
-        // show obat
-        // go to show obat html
-        // set value based on this
-    }
-    else if(obatDitemukan.length>1){
-        // show obat recomn
-        // go to show list obat
-        // set value based on this
-    }
-    else{
+    if (typeof obatDitemukan === 'string') {        
+        // tidak ditemukan
         // go to notfound html or something
         // show tidak ditemukan
+        alert('obat tidak ditemukan')
+        resetField()
     }
+    else if(Array.isArray(obatDitemukan)){
+        // show obat recommendation
+        // go to show list obat
+        // set value based on this
+        
+        render(obatDitemukan)
+    }
+    else{
+        // go to show obat html
+        // set value based on obatDitemukan
+        renderSpecific(obatDitemukan)
+    }
+}
+
+function render(obatList) {
+    // get todo list
+    let menu = document.getElementById("obatCardList")
+    let template = ''
+    // put all obat to html
+    for (const obat of obatList) {
+        template += `
+        <div class="card" style="width: 18rem;">
+            <img src="${obat.gambar}" class="card-img-top" alt="${obat.nama}">
+            <div class="card-body">
+                <p class="card-text">${obat.nama}\n${obat.manfaat}\n${obat.penyakit}</p>
+            </div>
+        </div>
+        `
+    }
+    // loop end
+    menu.innerHTML = template
+}
+
+
+function renderSpecific(obat) {
+    let menu = document.getElementById("obatCardList")
+    const template = `
+    <div class="card" style="width: 18rem;">
+        <img src="${obat.gambar}" class="card-img-top" alt="${obat.nama}">
+        <div class="card-body">
+            <p class="card-text">${obat.nama}\n${obat.manfaat}\n${obat.penyakit}</p>
+        </div>
+    </div>
+    `      
+    menu.innerHTML = template
+}
+
+// function gantiCard(){
+//     document.getElementById("obatCardList").innerHtml = ""
+//     render()
+// }
+
+function resetField() {
+    // clear field
+    document.getElementById('addNamaObat').value = ''
+    document.getElementById('addJenisObat').value = ''
+    document.getElementById('addIndexObat').value = ''
+    document.getElementById('addBentukObat').value = ''
+    document.getElementById('addWarnaObat').value = ''
+    document.getElementById('addManfaatObat').value = ''
+    document.getElementById('addGambar').value = ''
 }
